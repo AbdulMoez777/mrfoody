90112cebb427663a8c1b5201cfaeec1304a116da
Abdul Moez <abdulmoeztwitter@gmail.com>
2026-05-18 03:57:06 +0500

    Initial Commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="p-3 shadow-xl">
+      <div className="max-w-[1200px] bg-red-100 mx-auto">Hello</div>
+    </header>
+  );
+}
+
+export default Header;
